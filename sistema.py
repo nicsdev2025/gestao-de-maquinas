b41374f6210fd2aef0b6842135b30a3a4ead2086
nicsdev2025 <nicolasbatistadossantos06@gmail.com>
2026-03-29 14:42:27 -0300

    resolvi alguns erros

--- sistema.py
+++ sistema.py
@@ -33,22 +33,25 @@ while atualizar_tabela == "s":
     atualizar_patri = input("Digite o novo patrimonio: ")
     atualizar_hostname = input("Digite o novo hostname: ")
     atualizar_dia = input("digite o dia (AAAA-MM-DD):")
-    cursor.execute('UPDATE maquinas SET usuario = (%s), patrimonio = (%s) hostname = (%s) = dia = (%s)', (atualizar_usuario, atualizar_patri, atualizar_hostname, atualizar_dia,maquina_dia))
+    cursor.execute('UPDATE maquinas SET usuario = %s, patrimonio = %s, hostname = %s, dia = %s WHERE id = %s', (atualizar_usuario, atualizar_patri, atualizar_hostname, atualizar_dia,maquina_id))
     conexao.commit()
     print("Máquina atualizada com sucesso!")
     if atualizar_tabela != "s" and atualizar_tabela == "n":
         break
+    break
    
-
+# para deletar as máquinas
 delete_maquina = input("Deseja deletar alguma máquina?")
 while delete_maquina == "s":
     deletar_maquina = input("Digite o ID para deletar a máquina: ")
-    cursor.execute('DELETE FROM maquinas WHERE id = (%s)', (deletar_maquina))
+    cursor.execute('DELETE FROM maquinas WHERE id = (%s)', (deletar_maquina,))
     conexao.commit()
     cursor.close()
-    print(deletar_maquina)
+    print("Máquina deletada com sucesso!")
     if delete_maquina != "s" and deletar_maquina == "n":
         break
+    break
+
 verificacao = input("Você quer adicionar mais máquinas? s/n?: ")
 
 while verificacao == "s":
